45acb851c5d904c1bd2ce237648bcb2d0c6711ec
Schlagonia <brad@schlagelfamily.org>
2022-02-23 08:59:16 -0700

    Flasher

--- Bot/index.js
+++ Bot/index.js
@@ -1,12 +1,10 @@
 
 require('dotenv').config();
 
-//Gas Limit  BigNumber { value: "281752" }
-// gas limit 236630
 
 const privateKey = process.env.PRIVATE_KEY;
 // your contract address
-const flashLoanerAddress = process.env.FLASH_LOANER;
+const flashLoanerAddress = process.env.FLASH_LOANER1;
 
 const { ethers } = require('ethers');
 
@@ -23,18 +21,17 @@ let wallet = new ethers.Wallet(privateKey, provider);
 const joeFactoryAddress = '0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10'
 const pangolinFactoryAddress = '0xefa94DE7a4656D787667C749f7E1223D71E9FD88'
 
+//Token address to pass into pairAddress variable
 const daiAddress = '0xd586e7f844cea2f87f50152665bcbc2c279d8d70'; //dai.e
 const spellAddress = '0xce1bffbd5374dac86a2893119683f4911a2f7814';
-
 const yakAddress = '0x59414b3089ce2AF0010e7523Dea7E2b35d776ec7;'
 const clyAddress = '0xec3492a2508ddf4fdc0cd76f31f340b30d1793e6';
 const pefiAddress = '0xe896cdeaac9615145c0ca09c8cd5c25bced6384c';
-
 const iceAddress = '0xe0Ce60AF0850bF54072635e66E79Df17082A1109';
 const aaveAddress = '0x63a72806098bd3d9520cc43356dd78afe5d386d9';
 const pngAddress ='0x60781c2586d68229fde47564546784ab3faca982';
 
-//joe ptp and cra
+//Amounts that would be traded
 const AVAX_TRADE = 1;
 const AVAX_TRADE_FEE = AVAX_TRADE *.997
 const PAIR_TRADE = 215;
@@ -73,39 +70,23 @@ const runBot = async () => {
     try {
       console.log(blockNumber);
 
-      const joeReserves = await joePool.getReserves();
-      
+      const joeReserves = await joePool.getReserves();  
       const pangolinReserves = await pangolinPool.getReserves();
-      ///Resrve
+    
       const reserve0joe = Number(ethers.utils.formatUnits(joeReserves[0], 18));
-      
-      // reserve
       const reserve1joe = Number(ethers.utils.formatUnits(joeReserves[1], 18));
 
       const reserve0Pan = Number(ethers.utils.formatUnits(pangolinReserves[0], 18));
       const reserve1Pan = Number(ethers.utils.formatUnits(pangolinReserves[1], 18));
       
-        //PAIRToken / Avax 
       const pricePangolin = reserve1Pan / reserve0Pan;
       const priceJoe = reserve1joe / reserve0joe;
       
       const shouldStartAvax = pricePangolin > priceJoe;
       console.log("Start with AVAx? ", shouldStartAvax);
-      /*
-      const spread = Math.abs(priceJoe - pricePangolin)
-
-      let priceOut = (shouldStartAvax ? 
-        (AVAX_TRADE_FEE * reserve1Pan) / (AVAX_TRADE_FEE + reserve0Pan) / AVAX_TRADE
-        : PAIR_TRADE / ((PAIR_TRADE_FEE * reserve0Pan) / (PAIR_TRADE_FEE + reserve1Pan)) )
-      console.log("Price Out ", priceOut)
-      
-      // Calculate how much you will get out after fees and slippage
-      let amountOut = (shouldStartAvax ? 
-        (AVAX_TRADE_FEE * reserve1Pan) / (AVAX_TRADE_FEE + reserve0Pan) 
-        : ((PAIR_TRADE_FEE * reserve0Pan) / (PAIR_TRADE_FEE + reserve1Pan)) )
-      console.log("Amount Out ", amountOut)
-      */
-  
+   
+      //Takes what is needed to be put back in and determines how much would need to go into pangolin. 
+      // If what comes out of JOe is >  than what needs to go in Pang then its profitable
       // AVAX is TOKEN0
       let needs = (shouldStartAvax ? 
         (reserve1joe * AVAX_TRADE ) / ((reserve0joe - AVAX_TRADE) *.997 ) :
@@ -113,17 +94,16 @@ const runBot = async () => {
       )
       console.log("Needs ", needs)
       
-
       let panNeeded = (shouldStartAvax ? 
         (reserve0Pan * needs ) / ((reserve1Pan - needs) * .997 ) :
         (reserve1Pan * needs ) / ((reserve0Pan - needs) *.997 ) 
       )
       console.log("PanNeeded ", panNeeded)
+
         profit = shouldStartAvax ? (AVAX_TRADE - panNeeded) : (PAIR_TRADE - panNeeded)
       //Trade is profitable if out is greater than needed
       const shouldTrade = profit > 0
       
-      //the difference between what is pulled out of joe and what is need to put into Pan to get what is needed to go to back to Joe
       console.log("Profit ", profit);
       console.log(`Paangolin PRICE ${pricePangolin}`);
       console.log(`Joe PRICE ${priceJoe}`);
@@ -140,17 +120,13 @@ const runBot = async () => {
         flashLoanerAddress,
         ethers.utils.toUtf8Bytes('1'),
       );
-      console.log("Gas Limit ", Number(gasLimit))
 
       let gasPrice = await provider.getGasPrice();
       
       const gasCost = Number(ethers.utils.formatEther(gasPrice.mul(gasLimit)));
-      console.log("Gas Cost", gasCost)
 
       // need to compate gasCost in AVAx to whatever the profit is in
       const shouldSendTx = gasCost < (shouldStartAvax ?  profit : profit / priceJoe );
-      
-      console.log("compared to AVAX Profit of", (shouldStartAvax ?  profit : profit / priceJoe ))
 
       // don't trade if gasCost is higher than the spread
       if (!shouldSendTx) return;
